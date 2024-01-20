c1d1933cd56994944272b4a7ac752486e9f04dea
Shrimpy <s3978724@rmit.edu.vn>
2024-01-20 16:47:04 +0700

    update

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[]) {
                  "Enter your choice: ";
 
 int userType;
-    std::cin >> userType;
+std::cin >> userType;
 
 switch (userType) {
     case 1:
