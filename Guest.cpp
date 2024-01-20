db8bcc11c886d4fcd38bf6b5a990fcebc7e1ffef
Shrimpy <s3978724@rmit.edu.vn>
2024-01-21 02:38:03 +0700

    update

--- Guest.cpp
+++ Guest.cpp
@@ -1,34 +1,14 @@
 #include <iostream>
-#include "include/Admin.h"
 #include "include/Guest.h"
-#include "include/Member.h"
-#include "include/Request.h"
-#include "include/TimeBank.h"
-#include "include/User.h"
-#include <cstdlib> 
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <vector>
-#include <string>
 
 Guest::Guest(std::string &username, std::string &password) : User(username, password) {}
 
-std::string Guest::getUserName(){
+std::string Guest::getUserName()
+{
     return this->username;
 };
 
-std::string Guest::getPassWord(){
+std::string Guest::getPassWord()
+{
     return this->password;
 };
-
-
-bool Guest::memberIDExists(const std::string& memberId) const {
-    // Implementation to check if the member ID already exists in the system
-    // You may need to read from a file or query a database
-    // For this example, let's assume a simple check based on file existence
-    std::ifstream file(memberId + ".txt");
-    return file.good();
-}
-
-   
--- include/Guest.h
+++ include/Guest.h
@@ -1,25 +1,15 @@
 #pragma once
 
 #include "User.h"
-#include "Member.h"
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iomanip>
-#include <vector>
-#include <string>
 
-class Guest: public User
+class Guest : public User
 {
 public:
     Guest(std::string &username, std::string &password);
-
-    Member registerAsMember();
 
     std::string getUserName();
 
     std::string getPassWord();
 
 private:
-    bool memberIDExists(const std::string& memberId) const;
 };
