b6c81ad9cdb668c2724cfcd9603a80965029b4c6
Shrimpy <s3978724@rmit.edu.vn>
2024-01-20 17:18:03 +0700

    update

--- Member.cpp
+++ Member.cpp
@@ -1,5 +1,6 @@
 #include "include/Member.h"
 #include <vector>
+#include <fstream>
 
 using std::string, std::vector;
 
@@ -12,3 +13,45 @@ Member::Member(std::string &username, std::string &password,
 Member::Member(std::string &username, std::string &password)
     : User(username, password), fullName(""), phoneNumber(""),
       email(""), address(""), isSupporting(false) {}
+
+// SHOW THE ENTIRE MEMBERS FILE
+void Member::showAllInfo(string name){
+  string temp;
+  std::ifstream dataFile;
+  dataFile.open(name, std::ios::in); // read
+  if (!dataFile.is_open())
+  {
+    std::cout << "Fail to create/open file \n";
+  }
+  std::cout << " Here are the list of members: \n";
+  while (getline(dataFile, temp))
+  {
+    std::cout << temp << "\n";
+  }
+  dataFile.close();
+}
+
+// CLEAR ALL THE FILE CONTENT
+void Member::clearMember(std::string name)
+{
+  std::ofstream dataFile;
+  dataFile.open(name, std::ios::out);
+  dataFile << "";
+  dataFile.close();
+}
+
+// WRITE TO FILE
+bool Member::saveMember(std::string name)
+{
+  std::ofstream dataFile;
+  dataFile.open(name, std::ios::in | std::ios::app);
+  if (!dataFile.is_open())
+  {
+    std::cerr << "Fail to create/open file \n";
+    return false;
+  }
+  // Store in the file
+  dataFile.seekp(0, std::ios::end); // set the pointer back to end of file
+  dataFile.close();
+  return true;
+}
