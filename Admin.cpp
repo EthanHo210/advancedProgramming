38099139d9575ea2e19aeb0c5947b953cf8b87ce
laystr <bomby88888@gmail.com>
2024-01-21 19:00:10 +0700

    admin reset password function

--- Admin.cpp
+++ Admin.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include "include/Admin.h"
+#include "include/TimeBank.h"
 
 Admin::Admin(std::string username, std::string password) : User(username, password) {}
 
-void Admin::changeInfo(Member member)
-{
-}
-
-void Admin::resetPassword(Member member)
-{
+void Admin::resetPassword(const std::string username) {
+    TimeBank timeBank;
+    std::string newPassword;
+    std::cout << "Changing password for user: " << username << std::endl;
+    std::cout << "Enter the new password: ";
+    std::cin >> newPassword;
+    std::string path = "data/account/" + username + ".dat";
+    int lineNumber = 2;
+    timeBank.changeContentByLine(path, lineNumber, newPassword);
+    std::cout << "Password changed successfully for user: " << username << std::endl;
 }
--- include/Admin.h
+++ include/Admin.h
@@ -6,10 +6,7 @@
 class Admin : public User
 {
 public:
-    // Constructor
     Admin(std::string username, std::string password);
 
-    // Function to change data of a user
-    void changeInfo(Member member);
-    void resetPassword(Member member);
+    void resetPassword(const std::string username);
 };
